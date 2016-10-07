1d52c92c68505d9722500e20564454e7269b91d0
Ahmet Taspinar <taspinar@gmail.com>
2016-10-07 14:18:14 +0200

    update README.md

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup
 
 setup(
 		name='siml',
-		version='0.1',
+		version='0.1.2.1',
 		description='Machine Learning algorithms implemented from scratch',
 		url='https://github.com/taspinar/siml',
 		author='Ahmet Taspinar',
 		author_email='taspinar@gmail.com',
 		license='MIT',
 		packages=['siml'],
-		install_requires=[
-          'numpy', 
-		  'math'
-      ],
+		install_requires=['numpy'],
 		zip_safe=False
 		)

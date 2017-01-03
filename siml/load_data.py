c543dc678403ef06efe632c60942218bf9b539b9
Ahmet Taspinar <taspinar@gmail.com>
2017-01-03 15:03:48 +0100

    add method to normalize an array, method t o load myopia dataset

--- siml/load_data.py
+++ siml/load_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
+import numpy as np
+import random
 
+###
 def strip_quotations_newline(text):
     text = text.rstrip()
     if text[0] == '"':
@@ -21,8 +24,30 @@ def split_text(text):
     cleaned_text = [x for x in splitted_text if len(x)>1]
     text_lowercase = [x.lower() for x in cleaned_text]
     return text_lowercase
-  
 
+###
+def pow10(x):
+    i = 1;
+    while((i * 10) < x):
+        i *= 10.0;
+    return i
+    
+def normalize_col(col1, method):
+    cc_mean = np.mean(col1)
+    if method == 'pow10':         
+        return col1 / pow10(np.max(col1))
+    else:
+        return col1 - cc_mean
+    
+def normalize_matrix(X, method = 'mean'):
+    no_rows, no_cols = np.shape(X)
+    X_normalized = np.zeros(shape=(no_rows, no_cols))
+    X_normalized[:,0] = X[:,0]
+    for ii in range(1,no_cols):
+        X_normalized[:, ii] = normalize_col(X[:, ii], method)
+    return X_normalized    
+
+###
 def amazon_reviews():
     datafolder = '../datasets/amazon/'
     files = os.listdir(datafolder)
@@ -42,7 +67,6 @@ def amazon_reviews():
         f.close()
     return X_train, Y_train, X_test, Y_test
 
-
 def adult():
     datafile = '../datasets/adult/adult.data'
     file_test = '../datasets/adult/adult.test'
@@ -58,3 +82,30 @@ def adult():
     del df_test[2]
     X_test = df_test.values
     return X_train, Y_train, X_test, Y_test
+
+def myopia():
+    datafile = '../datasets/myopia.dat'
+    with open(datafile, 'r') as f:
+        lines = f.readlines()
+        random.shuffle(lines)
+        start_col = 2
+        no_lines = len(lines)
+        no_training_examples = int(0.7*no_lines)
+        no_test_examples = no_lines - no_training_examples
+        no_features = len(lines[0].split())-start_col
+        X, Y = np.zeros(shape=(no_lines, no_features)), np.zeros(shape=no_lines)
+        X[:,0] = 1
+        rownum = 0
+        for line in lines:
+            line = line.split()
+            line = map(float, line)
+            Y[rownum] = int(line[2])
+            X[rownum, 1:16] = line[3:18]
+            rownum+=1
+        X_norm = normalize_matrix(X, 'pow10')
+        X_train = X_norm[0:no_training_examples,:]
+        Y_train = Y[0:no_training_examples]
+        X_test = X_norm[no_training_examples:no_lines,:]
+        Y_test = Y[no_training_examples:no_lines]
+    return X_train, Y_train, X_test, Y_test
+        

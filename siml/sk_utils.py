9afe0a0d93956489bba3b34fb07f0153575918f7
Ahmet Taspinar <taspinar@gmail.com>
2017-11-21 11:57:29 +0100

    add file containing handy function using the scikit-learn library

---
+++ siml/sk_utils.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+import time
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+###
+
+dict_classifiers = {
+    "Gradient Boosting Classifier": GradientBoostingClassifier(),
+    "Random Forest": RandomForestClassifier(n_estimators = 18),
+    "Logistic Regression": LogisticRegression(),
+    "Nearest Neighbors": KNeighborsClassifier(),
+    "Linear SVM": SVC(),
+    "Neural Net": MLPClassifier(alpha = 1),
+    "Naive Bayes": GaussianNB(),   
+    "Decision Tree": tree.DecisionTreeClassifier(),
+    "AdaBoost": AdaBoostClassifier(),
+    "QDA": QuadraticDiscriminantAnalysis(),
+    "Gaussian Process": GaussianProcessClassifier()
+}
+   
+def batch_classify(X_train, Y_train, X_test, Y_test, no_classifiers = 5, verbose = True):
+    """
+    This method, takes as input the X, Y matrices of the Train and Test set.
+    And fits them on all of the Classifiers specified in the dict_classifier.
+    Usually, the SVM, Random Forest and Gradient Boosting Classifier take quiet some time to train. 
+    So it is best to train them on a smaller dataset first and 
+    decide whether you want to comment them out or not based on the test accuracy score.
+    """
+    
+    dict_models = {}
+    for classifier_name, classifier in list(dict_classifiers.items())[:no_classifiers]:
+        t_start = time.clock()
+        classifier.fit(X_train, Y_train)
+        t_end = time.clock()
+        
+        t_diff = t_end - t_start
+        train_score = classifier.score(X_train, Y_train)
+        test_score = classifier.score(X_test, Y_test)
+        
+        dict_models[classifier_name] = {'model': classifier, 'train_score': train_score, 'test_score': test_score, 'train_time': t_diff}
+        if verbose:
+            print("trained {c} in {f:.2f} s".format(c=classifier_name, f=t_diff))
+    return dict_models
+
+####
+
+def label_encode(df, list_columns):
+    """
+    This method one-hot encodes all column, specified in list_columns
+    
+    """
+    for col in list_columns:
+        le = LabelEncoder()
+        col_values_unique = list(df[col].unique())
+        le_fitted = le.fit(col_values_unique)
+
+        col_values = list(df[col].values)
+        le.classes_
+        col_values_transformed = le.transform(col_values)
+        df[col] = col_values_transformed
+
+def get_train_test(df, y_col, x_cols, ratio):
+    """ 
+    This method transforms a dataframe into a train and test set, for this you need to specify:
+    1. the ratio train : test (usually 0.7)
+    2. the column with the Y_values
+    """
+    mask = np.random.rand(len(df)) < ratio
+    df_train = df[mask]
+    df_test = df[~mask]
+       
+    Y_train = df_train[y_col].values
+    Y_test = df_test[y_col].values
+    X_train = df_train[x_cols].values
+    X_test = df_test[x_cols].values
+    return df_train, df_test, X_train, Y_train, X_test, Y_test
+    
+def display_dict_models(dict_models, sort_by='test_score'):
+    cls = [key for key in dict_models.keys()]
+    test_s = [dict_models[key]['test_score'] for key in cls]
+    training_s = [dict_models[key]['train_score'] for key in cls]
+    training_t = [dict_models[key]['train_time'] for key in cls]
+    
+    df_ = pd.DataFrame(data=np.zeros(shape=(len(cls),4)), columns = ['classifier', 'train_score', 'test_score', 'train_time'])
+    for ii in range(0,len(cls)):
+        df_.loc[ii, 'classifier'] = cls[ii]
+        df_.loc[ii, 'train_score'] = training_s[ii]
+        df_.loc[ii, 'test_score'] = test_s[ii]
+        df_.loc[ii, 'train_time'] = training_t[ii]
+    
+    display(df_.sort_values(by=sort_by, ascending=False))
+    
+def display_corr_matrix(df):
+    correlation_matrix = df.corr()
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax = sns.heatmap(correlation_matrix, vmax=1, square=True,annot=False,cmap='RdYlGn')
+    plt.title('Correlation matrix between the features')
+    plt.colorbar(fig, orientation='vertical', fraction=0.03, label = 'Correlation strength')
+    plt.show()
+
+def display_corr_with_col(df, col):
+    correlation_matrix = df.corr()
+    correlation_type = correlation_matrix[col].copy()
+    abs_correlation_type = correlation_type.apply(lambda x: abs(x))
+    desc_corr_values = abs_correlation_type.sort_values(ascending=False)
+    y_values = list(desc_corr_values.values)[1:]
+    x_values = range(0,len(y_values))
+    xlabels = list(desc_corr_values.keys())[1:]
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.bar(x_values, y_values)
+    ax.set_title('The correlation of all features with {}'.format(col), fontsize=20)
+    ax.set_ylabel('Pearson correlatie coefficient [abs waarde]')
+    plt.xticks(x_values, xlabels, rotation='vertical')
+    plt.show()

b8cb92994ef49b246cf7f8baeb4c95ff173779fc
Ahmet Taspinar <taspinar@gmail.com>
2017-11-29 11:37:20 +0100

    update version no and changelog

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
 		name='siml',
-		version='0.1.2.1',
+		version='0.2.1',
 		description='Machine Learning algorithms implemented from scratch',
 		url='https://github.com/taspinar/siml',
 		author='Ahmet Taspinar',
 		author_email='taspinar@gmail.com',
 		license='MIT',
 		packages=['siml'],
-		install_requires=['numpy'],
+		install_requires=['numpy', 'scikit-learn'],
 		zip_safe=False
 		)

4628f31e7fd08f9af2b831fccc6191295c7b6186
Ahmet Taspinar <taspinar@gmail.com>
2020-04-08 21:41:25 +0200

    Update __init__.py

--- siml/__init__.py
+++ siml/__init__.py
@@ -1,11 +1,11 @@
 # siML
-# Copyright 2016-2017 Ahmet Taspinar
+# Copyright 2016-2020 Ahmet Taspinar
 # See LICENSE for details.
 
 """
 Library implementing Machine Learning algorithms from scratch
 """
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Ahmet Taspinar'
 __license__ = 'MIT'
 
